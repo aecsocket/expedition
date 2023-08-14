869d3eaea0ae1839b0d104607a47c87b0941951a
aecsocket <aecsocket@tutanota.com>
2023-08-14 18:13:53 +0100

    Work on docs

--- src/text.rs
+++ src/text.rs
@@ -5,12 +5,100 @@ use crate::prelude::*;
 // core types
 
 /// Represents a generic rich text message, designed to be used as the intermediary format for
-/// text manipulation. This provides simple yet universal rich-text features
+/// text manipulation.
+/// 
+/// This provides rich text features such as decoration and coloring, which you can easily create
+/// yourself via the builder-style APIs. The features available are designed to be simple and
+/// universal, so that as many different output modes as possible can be supported. For example,
+/// text can be converted to:
+/// 
+/// - a raw string
+/// - ANSI codes for printing to a terminal
+/// - a [`LayoutJob`] for [`egui`]
+/// 
+/// The messages are stored in a tree structure, with one message capable of holding child nodes,
+/// which can be traversed and used for applying styling on top of existing styling.
+/// 
+/// # Examples
+/// 
+/// ```
+/// use expedition::prelude::*;
+/// 
+/// // Create a new Text using `new`, passing an `Into<String>`
+/// let text1 = Text::new("Hello world!");
+/// 
+/// // Or get one directly from a string
+/// let text2 = "Hello world!".into_text();
+/// assert_eq!(text1, text2);
+/// 
+/// // Apply styling using a builder pattern
+/// let text1 = Text::new("Unstyled text, ")
+///     .with(Text::new("red text, ").color(Color32::RED))
+///     .with(Text::new("blue text").color(Color32::BLUE));
+/// 
+/// // Which can be simplified to
+/// let text2 = "Unstyled text, "
+///     .with("red text, ".color(Color32::RED))
+///     .with("blue text".color(Color32::BLUE));
+/// assert_eq!(text1, text2);
+/// 
+/// // Add decorations such as bold, italic, underline, and strikethrough
+/// let text = "Unstyled, "
+///     .with("bold text, ".bold())
+///     .with("italic text, ".italic())
+///     .with("all the decorations".bold().italic().underline().strikethrough());
+/// 
+/// // Styling from child nodes takes priority over parent nodes
+/// let text = "Red text, ".color(Color32::RED)
+///     .with("still red text, ")
+///     .with("red and italic, ".italic())
+///     .with("blue and not italic".color(Color32::BLUE));
+/// 
+/// // Or use `no_X()` to disable the decoration `X`
+/// let text = "Italic text, ".italic()
+///     .with("not italic anymore".no_italic());
+/// ```
 #[derive(Clone, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Text {
+    /// What text this object holds.
     pub content: String,
+    /// Decoration and formatting applied to this text message.
     pub style: TextStyle,
+    /// Child text messages added on to this text.
     pub children: Vec<Text>,
+}
+
+/// Styling that is currently applied to the contents of a [`Text`].
+/// 
+/// All styling elements are optional, as styles can be layered on top of one another through
+/// merging. Because of this, [`Default`] returns a style object that applies no styling changes
+/// to a piece of text - effectively an "identity style".
+#[derive(Clone, PartialEq, Eq, Hash, Default)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct TextStyle {
+    /// Foreground text color.
+    pub color: Option<Color32>,
+    /// Bold decoration.
+    pub bold: StyleState,
+    /// Italic decoration.
+    pub italic: StyleState,
+    /// Underline decoration.
+    pub underline: StyleState,
+    /// Strikethrough decoration.
+    pub strikethrough: StyleState,
+}
+
+/// A tri-state for if a piece of styling should be applied.
+/// 
+/// Since styles can be inherited from 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum StyleState {
+    #[default]
+    Inherit,
+    On,
+    Off,
 }
 
 impl Text {
@@ -21,15 +109,6 @@ impl Text {
             children: Vec::new(),
         }
     }
-}
-
-#[derive(Clone, PartialEq, Eq, Hash, Default)]
-pub struct TextStyle {
-    pub color: Option<Color32>,
-    pub bold: StyleState,
-    pub italic: StyleState,
-    pub underline: StyleState,
-    pub strikethrough: StyleState,
 }
 
 impl TextStyle {
@@ -64,14 +143,6 @@ impl TextStyle {
         res.merge_from(from);
         res
     }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
-pub enum StyleState {
-    #[default]
-    Inherit,
-    On,
-    Off,
 }
 
 // text traits
